fdca009cfbeca73545daf8968e65b5494d547148
mancari-jo <168064864+mancari-jo@users.noreply.github.com>
2024-05-17 08:32:55 +0800

    Add files via upload

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 
 export default class Config {
   static HTTP_PORT = process.env.HTTP_PORT || 3001;
-  static DATABASE_URL = `${process.env.DATABASE_URL}`;
+  static DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/mancari-jo';
 }
